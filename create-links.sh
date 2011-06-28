c9d3841b65ef181f11c75d9c103ae6dc70aa4434
Chris Lyons <clyons@berklee.edu>
2011-06-28 10:59:18 -0400

    hack shell script

---
+++ create-links.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# create hack symlinks for dot files in ~/dot-files/
+#
+# clone 'dot-files' into ~/, chmod +x this file, and run
+#
+
+# eventually just make this read directory contents. fine for now
+files=(".vim" 
+       ".vimrc")
+
+# change into home dir
+cd ~/
+
+# create symlinks. this will error out if files already exist
+for i in ${files[@]}
+do
+  ln -s ~/dot-files/$i $i
+done
